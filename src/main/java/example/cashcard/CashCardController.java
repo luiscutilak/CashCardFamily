7f6269751f722295e8332caec57165c11f4105f6
Luis Fernando Cutilak <luiscutilak@gmail.com>
2023-10-24 12:44:49 -0300

    explanations cashcard implem. controller

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -6,12 +6,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/*
+ * Vamos atualizar nossa classe CashCardController para que ele esteja configurado para escutar e lidar com solicitações HTTP
+ * inserindo as Annotations.
+ */
 @RestController
 @RequestMapping("/cashcards")
 
 public class CashCardController {
 
     @GetMapping("/{requestId}")
+    /*
+     1º Adicionando método manipulador(handler method), findById 
+    para lidar com solicitações HTTP recebidas.
+     */ 
     public ResponseEntity<CashCard> findById(@PathVariable Long requestId) {
         if (requestId.equals(99L)) {
             CashCard cashCard = new CashCard(99L, 123.45);
--- src/test/resources/example/cashcard/CashcardApplicationTests.java
+++ src/test/resources/example/cashcard/CashcardApplicationTests.java
@@ -9,15 +9,25 @@ import org.springframework.http.ResponseEntity;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+//Isso iniciará nosso aplicativo Spring Boot e o disponibilizará para nosso teste realizar solicitações a ele.
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CashcardApplicationTests {
-	@Autowired
 
+	// Pedimos ao Spring para injetar um auxiliar de teste que nos permitirá fazer solicitações HTTP para o aplicativo em execução localmente.
+	@Autowired
 	TestRestTemplate restTemplate;
+
 	@Test
 	public void contextLoads() {
+
+		// Aqui usamos restTemplate para fazer uma solicitação HTTP GET para o endpoint do nosso aplicativo /cashcards/99.
+
+		/*restTemplate retornará um ResponseEntity, que capturamos em uma variável que chamamos de resposta. ResponseEntity é outro objeto Spring útil que fornece informações valiosas sobre o que aconteceu com nossa solicitação.
+		*/
+
 		ResponseEntity<String> response = restTemplate.getForEntity("/cashcard99", String.class);
 		
+		//Podemos inspecionar muitos aspectos da resposta, incluindo o código de status da resposta HTTP, que esperamos ser 200 OK.
 		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
 	}
 
